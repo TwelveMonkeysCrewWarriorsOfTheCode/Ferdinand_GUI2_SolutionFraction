15a3867f32b60780ae504a6087c86693d5a55180
Ferdinand Teguefouet <teguefouet1@gmail.com>
2021-08-03 14:58:40 +0200

    Ajoutez des fichiers projet.

---
+++ FractionBLL/Fraction.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace FractionBLL
+{
+    public class Fraction
+    {
+        private int m_Numerator;
+        private int m_Denominator;
+
+        public int Numerator
+        {
+            get { return m_Numerator; }
+            set { m_Numerator = value; }
+        }
+        public int Denominator
+        {
+            get { return m_Denominator; }
+            set { m_Denominator = value; }
+        }
+
+        public Fraction(int pNumber) => InitializeFraction(pNumber, 1);
+
+        public Fraction(Fraction pFraction) => InitializeFraction(pFraction.m_Numerator, pFraction.m_Denominator);
+        public Fraction(int pNumerator, int pDenominator)
+        {
+            if (pDenominator == 0) throw new DivideByZeroException("Le diviseur ne peut pas être égal à zéro.");
+            InitializeFraction(pNumerator, pDenominator);
+        }
+
+        public static Fraction operator +(Fraction pFraction1, Fraction pFraction2)
+        {
+            int num = NumeratorLeftOperation(pFraction1, pFraction2) + NumeratorRightOperation(pFraction1, pFraction2);
+
+            return new Fraction(num, CalculateDeno(pFraction1, pFraction2));
+        }
+
+        public static Fraction operator -(Fraction pFraction1, Fraction pFraction2)
+        {
+            int num = NumeratorLeftOperation(pFraction1, pFraction2) - NumeratorRightOperation(pFraction1, pFraction2);
+            return new Fraction(num, CalculateDeno(pFraction1, pFraction2));
+        }        
+
+        public static Fraction operator *(Fraction pFraction1, Fraction pFraction2)
+        {
+            int num = pFraction1.m_Numerator * pFraction2.m_Numerator;
+            return new Fraction(num, CalculateDeno(pFraction1, pFraction2));
+        }
+
+        public static Fraction operator /(Fraction pFraction1, Fraction pFraction2)
+        {
+            int num = pFraction1.m_Numerator * pFraction2.m_Denominator;
+            int deno = pFraction1.m_Denominator * pFraction2.m_Numerator;
+
+            return new Fraction(num, deno);
+        }
+
+        private static int NumeratorRightOperation(Fraction pFraction1, Fraction pFraction2) =>
+            pFraction1.m_Denominator * pFraction2.m_Numerator;
+        private static int NumeratorLeftOperation(Fraction pFraction1, Fraction pFraction2) =>
+            pFraction1.m_Numerator * pFraction2.m_Denominator;
+        private static int CalculateDeno(Fraction pFraction1, Fraction pFraction2) =>
+            pFraction1.m_Denominator * pFraction2.m_Denominator;
+        private void InitializeFraction(int pNum, int pDeno)
+        {
+            this.m_Numerator = pNum;
+            this.m_Denominator = pDeno;
+            SimplifyFraction();
+        }        
+
+        private void SimplifyFraction()
+        {
+            if (this.m_Denominator < 0)
+            {
+                this.m_Numerator = -this.m_Numerator;
+                this.m_Denominator = -this.m_Denominator;
+            }
+            int myDCD = GCD(this.m_Numerator, this.m_Denominator);
+            this.m_Numerator /= myDCD;
+            this.m_Denominator /= myDCD;
+
+        }
+        private int GCD(int pN1, int pN2)
+        {
+            while (pN2 != 0)
+            {
+                int temp = pN2;
+                pN2 = pN1 % pN2;
+                pN1 = temp;
+            }
+            return pN1;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (obj.GetType() != typeof(Fraction)) return false;
+            Fraction fraction = obj as Fraction;
+            if (fraction == null)
+            {
+                return false;
+            }
+            return this.m_Numerator == fraction.m_Numerator && this.m_Denominator == fraction.m_Denominator;
+        }
+
+        public override int GetHashCode() => this.m_Numerator.GetHashCode() ^ this.m_Denominator.GetHashCode();
+
+        // Substitution de ToString() afin de retourner la valeur d'une fraction comme une chaine
+        public override string ToString() => m_Numerator.ToString() + "/" + m_Denominator.ToString();
+
+    }
+}
---
+++ UnitTestProjectFraction/UnitTestFraction.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FractionBLL;
+
+namespace UnitTestProjectFraction
+{
+    [TestClass]
+    public class UnitTestFraction
+    {
+        [DataRow(2, -2, 4, -2, -3, 1, DisplayName = "Addition de (2/-2) et (4/-2)")]
+        [DataRow(1, -2, 1, -2, -1, 1, DisplayName = "Addition de (1/-2) et (1/-2)")]
+        [DataRow(2, 2, 4, 2, 3, 1, DisplayName = "Addition de (2/2) et (4/2)")]
+        [DataRow(2, 5, 2, 4, 9, 10, DisplayName = "Addition de (2/5) et (2/4)")]
+        [TestMethod]
+        public void NormalFraction(int pNum1, int pDeno1, int pNum2, int pDeno2, int pExpectedNum, int pExpectedDeno)
+        {                  
+            Fraction ExpectedObject = new Fraction(pExpectedNum, pExpectedDeno);
+            Fraction F1 = new Fraction(pNum1, pDeno1);
+            Fraction F2 = new Fraction(pNum2, pDeno2);
+            Fraction result = F1 + F2;
+
+            Assert.AreEqual(ExpectedObject, result);            
+        }
+
+        [DataRow(0, 3, 3, 1, DisplayName = "Addition de 0 et 3 --> fraction nulle")]
+        [DataRow(-1, 2, 1, 1, DisplayName = "Addition de -1 et 2")]
+        [DataRow(2, 3, 5, 1, DisplayName = "Addition de 2 et 3")]
+        [TestMethod]
+        public void FractionWithoutDenominator(int pNumber1, int pNumber2, int pExpectedNum, int pExpectedDeno)
+        {
+            Fraction fract1 = new Fraction(pNumber1);
+            Fraction fract2 = new Fraction(pNumber2);            
+            Fraction ExpectedObject = new Fraction(pExpectedNum, pExpectedDeno);
+            Fraction Result = fract1 + fract2;
+
+            Assert.AreEqual(ExpectedObject, Result);
+        }
+
+        [DataRow(0, 1, 2, 1, 2, DisplayName = "Addition de 0 et 1/2")]
+        [DataRow(2, 1, 2, 5, 2, DisplayName = "Addition de 2 et 1/2")]
+        [DataRow(1, 3, 7, 10, 7, DisplayName = "Addition de 1 et 3/7")]
+        [TestMethod]
+        public void AddNumberWithFraction(int pNumber, int pNumerator, int pDenominator, int pExpectedNum, int pExpectedDeno)
+        {
+            Fraction fract1 = new Fraction(pNumber);
+            Fraction fract2 = new Fraction(pNumerator, pDenominator);
+            Fraction ExpectedObject = new Fraction(pExpectedNum, pExpectedDeno);
+            Fraction Result = fract1 + fract2;
+
+            Assert.AreEqual(ExpectedObject, Result);
+        }
+
+        [DataRow(1, 2, 1, 4, 1, 4, DisplayName = "Soustraire (1/2) et (1/4)")]
+        [DataRow(2, 5, 3, 4, -7, 20, DisplayName = "Soustraire (2/5) et (3/4)")]
+        [TestMethod]
+        public void SubtractTwoFractions(int pNum1, int pDeno1, int pNum2, int pDeno2, int pExpectedNum, int pExpectedDeno)
+        {
+            Fraction ExpectedObject = new Fraction(pExpectedNum, pExpectedDeno);
+            Fraction F1 = new Fraction(pNum1, pDeno1);
+            Fraction F2 = new Fraction(pNum2, pDeno2);
+            Fraction result = F1 - F2;
+
+            Assert.AreEqual(ExpectedObject, result);
+        }
+
+        [DataRow(-1, 2, 3, 4, -3, 8, DisplayName = "Multiplier (-1/2) et (3/4)")]
+        [DataRow(2, 5, 3, 4, 3, 10, DisplayName = "Multiplier (2/5) et (3/4)")]
+        [TestMethod]
+        public void MultiplyTwoFractions(int pNum1, int pDeno1, int pNum2, int pDeno2, int pExpectedNum, int pExpectedDeno)
+        {
+            Fraction ExpectedObject = new Fraction(pExpectedNum, pExpectedDeno);
+            Fraction F1 = new Fraction(pNum1, pDeno1);
+            Fraction F2 = new Fraction(pNum2, pDeno2);
+            Fraction result = F1 * F2;
+
+            Assert.AreEqual(ExpectedObject, result);
+        }
+
+        [DataRow(-2, 5, 3, 4, -8, 15, DisplayName = "Diviser (-2/5) et (3/4)")]
+        [DataRow(2, 5, 3, 4, 8, 15, DisplayName = "Diviser (2/5) et (3/4)")]
+        [TestMethod]
+        public void DivideTowFractions(int pNum1, int pDeno1, int pNum2, int pDeno2, int pExpectedNum, int pExpectedDeno)
+        {
+            Fraction ExpectedObject = new Fraction(pExpectedNum, pExpectedDeno);
+            Fraction F1 = new Fraction(pNum1, pDeno1);
+            Fraction F2 = new Fraction(pNum2, pDeno2);
+            Fraction result = F1 / F2;
+
+            Assert.AreEqual(ExpectedObject, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivideIntNumberByZero()
+        {
+            Fraction f = new Fraction(2, 0);
+            //Assert.AreEqual(2, f.Numerator);
+            //Assert.AreEqual(0, f.Denominator);
+        }
+        
+    }
+}
